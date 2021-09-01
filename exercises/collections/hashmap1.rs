914eaf504f1c1763d74d14214fc80dd27d7228fd
hi-rustin <rustin.liu@gmail.com>
2021-09-01 11:17:21 +0800

    Finish collctions
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,17 +11,18 @@
 // Execute the command `rustlings hint hashmap1` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
-
-    // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("banana1"), 2);
+    basket.insert(String::from("banana2"), 2);
+    basket.insert(String::from("banana3"), 2);
+    basket.insert(String::from("banana4"), 2);
+    basket.insert(String::from("banana5"), 2);
 
     basket
 }
